cd379797a32b85bd7c155ac9c315af3a382d4aa5
Michael Green <mcg520@nyu.edu>
2017-02-18 14:37:25 -0500

    Simple Signs

---
+++ Assets/Scripts/NonEnemyNPC.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonEnemyNPC : MonoBehaviour {
+
+	public string Name;
+	public string Dialogue;
+	public DialogueManager diamang;
+	
+	void OnCollisionEnter2D(Collision2D other) {
+		Debug.Log ("here");
+		diamang.ShowBox(this.name, this.Dialogue );
+	}
+}
