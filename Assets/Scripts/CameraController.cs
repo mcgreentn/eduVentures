911906cbee8716014f9d2dcb64bcdb30b175f5ed
Andrew-Bo-Lin <andrewbolin.15@gmail.com>
2017-02-18 08:50:11 -0500

    Tracking Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 	public GameObject Player;
 	private Vector3 Offset;
+    public BoxCollider2D boundBox;
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
+    private Camera theCamera;
+    private float halfHeight;
+    private float halfWidth;
+
 	// Use this for initialization
 	void Start () {
 		//calc Offset
 		Offset = transform.position - Player.transform.position;
+        minBounds = boundBox.bounds.min;
+        maxBounds = boundBox.bounds.max;
+
+        theCamera = GetComponent<Camera>();
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
+
 	}
 
 	// Update is called once per frame
 	void LateUpdate () {
 		// Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         transform.position = Player.transform.position + Offset;
-	}
+        float clampedX = Mathf.Clamp(transform.position.x,minBounds.x + halfWidth, maxBounds.x - halfWidth);
+        float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+        transform.position = new Vector3(clampedX,clampedY,transform.position.z);
+    }
 }
