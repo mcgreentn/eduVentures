e6ada26338742f15b002949878ccae43adb0dd65
Andrew-Bo-Lin <andrewbolin.15@gmail.com>
2017-02-18 14:10:38 -0500

    Journal For Reals this time?

---
+++ Assets/Scripts/JournalHolder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JournalHolder : MonoBehaviour {
+    public string journal;
+    private JournalScriptManager jman;
+    public string[] dialogueLines;
+	// Use this for initialization
+	void Start () {
+        jman = FindObjectOfType<JournalScriptManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            if (!jman.journalActive)
+            {
+                jman.dialogLines = dialogueLines;
+                jman.currentLine = 0;
+                jman.ShowDialogue();
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Here");
+        if(other.gameObject.name == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                if (!jman.journalActive)
+                {
+                    jman.dialogLines = dialogueLines;
+                    jman.currentLine = 0;
+                    jman.ShowDialogue();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/JournalScriptManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class JournalScriptManager : MonoBehaviour {
+    public GameObject jBox;
+    public Text jtext;
+    public bool journalActive;
+    public string[] dialogLines;
+    public int currentLine;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    // Update is called once per frame
+    void Update() {
+        jtext.text = dialogLines[currentLine];
+        if (journalActive && Input.GetKeyDown(KeyCode.Space)) {
+            currentLine += 1;
+            //jBox.SetActive(false);
+            //journalActive = false;
+            if(currentLine>= dialogLines.Length)
+            {
+                jBox.SetActive(false);
+                journalActive = false;
+                currentLine = 0;
+            }
+            jtext.text = dialogLines[currentLine];
+                }
+     //  if (!journalActive)
+       // {
+         //   jBox.SetActive(false);
+        //}
+    }
+    public void ShowBox(string dialogue)
+    {
+        journalActive = true;
+        jBox.SetActive(true);
+        jtext.text = dialogue;
+    }
+    public void ShowDialogue()
+    {
+        journalActive = true;
+        jBox.SetActive(true);
+        
+    }
+}
