cc387da5052533c929b4abc454c0ef2a02b8f493
Andrew-Bo-Lin <andrewbolin.15@gmail.com>
2017-02-18 21:35:55 -0500

    End Card

---
+++ Assets/Scripts/Percentage_Right_Writer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Percentage_Right_Writer : MonoBehaviour
+{
+    public Text percent;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        percent.text = "You got a: " + (GameStats.QuestionsCorrect/GameStats.QuestionsAsked *100f) + "%" ;
+    }
+}
